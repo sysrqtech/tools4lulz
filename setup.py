e3879d20db9751251e6681d8ff722566936766cb
CyberTailor <CyberTailor@gmail.com>
2016-09-02 17:25:04 +0500

    missing deps

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(name='SysRq tools4lulz',
       version='rofling-release',
-      description='Collection of some fun services.',
+      description='Different tools for SysRq Technologies website.',
       author='SysRq Technologies',
       author_email='cybertailor@gmail.com',
       url='https://sysrq.tech',
-      install_requires=['Flask', 'vk', 'raven', 'Pillow', 'imgurpython'])
+      install_requires=['Flask', 'vk', 'raven[flask]', 'Pillow', 'imgurpython'])
